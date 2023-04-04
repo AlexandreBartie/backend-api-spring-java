7fd842df5d43004f2e233d22cac39a125cbe0307
Alexandre Bartie <bartie.devops@outlook.com>
2023-04-03 23:49:18 -0300

    refact: change name to ServerAPI

--- src/main/java/br/com/bartie/backendapispringjava/serverAPI.java
+++ src/main/java/br/com/bartie/backendapispringjava/serverAPI.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApiSpringJavaApplication {
+public class serverAPI {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApiSpringJavaApplication.class, args);
+		SpringApplication.run(serverAPI.class, args);
 	}
 
 }
--- src/test/java/br/com/bartie/backendapispringjava/ServerAPITests.java
+++ src/test/java/br/com/bartie/backendapispringjava/ServerAPITests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApiSpringJavaApplicationTests {
+class ServerAPITests {
 
 	@Test
 	void contextLoads() {
