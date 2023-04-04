0fcd95bf4665328d0c1ff9799c2ddf2cc5e2b7e1
Alexandre Bartie <bartie.devops@outlook.com>
2023-04-03 23:40:23 -0300

    feat: create project settings

---
+++ src/main/java/br/com/bartie/backendapispringjava/BackendApiSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.bartie.backendapispringjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApiSpringJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bartie/backendapispringjava/BackendApiSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bartie.backendapispringjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
