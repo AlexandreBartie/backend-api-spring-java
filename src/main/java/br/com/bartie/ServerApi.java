4900e2a241fc9178f1d479d92c0f3f9a0ccba52c
Alexandre Bartie <bartie.devops@outlook.com>
2023-04-07 00:59:15 -0300

    fix: class packages

--- src/main/java/br/com/bartie/ServerApi.java
+++ src/main/java/br/com/bartie/ServerApi.java
@@ -1,4 +1,4 @@
-package br.com.bartie.controllers;
+package br.com.bartie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
