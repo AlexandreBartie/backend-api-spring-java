af9f4a04800a60cd87c97e4cdf4515df2a840637
Alexandre Bartie <bartie.devops@outlook.com>
2023-04-12 22:58:55 -0300

    feat: Person API V2 json settings

--- src/main/java/br/com/bartie/api/v2/view/PersonDTO.java
+++ src/main/java/br/com/bartie/api/v2/view/PersonDTO.java
@@ -3,6 +3,10 @@ package br.com.bartie.api.v2.view;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,15 +16,23 @@ import lombok.ToString;
 @Setter
 @EqualsAndHashCode
 @ToString
+@JsonPropertyOrder({"id", "first_name", "last_name", "birthday", "address"})
 public class PersonDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+
+	@JsonProperty("first_name")
 	private String firstName;
+
+	@JsonProperty("last_name")
 	private String lastName;
-	private String address;
+
+	@JsonIgnore()
 	private String gender;
+
+	private String address;
 	private Date birthday;
 
 }
