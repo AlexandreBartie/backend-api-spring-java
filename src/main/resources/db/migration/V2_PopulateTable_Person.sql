1241cfdd62ec03a1ffd6dbcedd7ad309df822895
Alexandre Bartie <bartie.devops@outlook.com>
2023-04-12 22:10:08 -0300

    feat: migration SQL

---
+++ src/main/resources/db/migration/V2_PopulateTable_Person.sql
@@ -0,0 +1,25 @@
+INSERT INTO Java.person (first_name,last_name,address,gender) VALUES
+	 ('Alex#1','Bartie#1','Adress#New','#New'),
+	 ('Alex#2','Bartie#2','Adress#New','#New'),
+	 ('FistName#Changed','LastName#Changed','Adress#Changed','#Changed'),
+	 ('FistName#Changed','LastName#Changed','Adress#Changed','#Changed'),
+	 ('FistName#Changed','LastName#Changed','Adress#Changed','Changed'),
+	 ('FistName#Changed','LastName#Changed','Adress#Changed','Changed'),
+	 ('FistName#Changed','LastName#Changed','Adress#Changed','Changed'),
+	 ('FistName#Created','LastName#Created','Adress#Created','Created'),
+	 ('FistName#Created','LastName#Created','Adress#Created','Created'),
+	 ('FistName#Created','LastName#Created','Adress#Created','Created');
+INSERT INTO Java.person (first_name,last_name,address,gender) VALUES
+	 ('FistName#Created','LastName#Created','Adress#Created','Created'),
+	 ('FistName#Created','LastName#Created','Adress#Created','Created'),
+	 ('Dedrick','McCullough','{{$randomAddress}}','Male'),
+	 ('Flavie','Cassin','75376 Ziemann Oval','Male'),
+	 ('Edwin','Mitchell','Sarah Courts','Male'),
+	 ('Brent','King','3145 Joyce Alley, 400, Godfreyborough, Togo','Male'),
+	 ('Jacinthe','Shanahan','4470 Hintz Valley, 570, Margaritaview, Canada','Male'),
+	 ('Felix','Greenfelder','17512 Bartholome Loop, 643, South Ryann, Eritrea','Male'),
+	 ('John','Kuphal','6403 Pagac Islands, 689, Macon-Bibb County, Colombia','Male'),
+	 ('FistName#Changed','LastName#Changed','Adress#Changed','Changed');
+INSERT INTO Java.person (first_name,last_name,address,gender) VALUES
+	 ('FistName#Changed','LastName#Changed','Adress#Changed','Changed'),
+	 ('FistName#Changed','LastName#Changed','Adress#Changed','Changed');
