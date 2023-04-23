3dff85d5faa96e35975feccdde05ccd46a70c996
Alexandre Bartie <bartie.devops@outlook.com>
2023-04-23 00:39:07 -0300

    fix: add ID in API results

--- src/main/java/br/com/bartie/app/core/ModelDTO.java
+++ src/main/java/br/com/bartie/app/core/ModelDTO.java
@@ -6,23 +6,29 @@ import org.springframework.hateoas.RepresentationModel;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class ModelDTO<DTO extends RepresentationModel<? extends DTO>> extends RepresentationModel<DTO> implements Serializable {    
 
     private static final long serialVersionUID = 1L;
 
-	protected Long id;
+	public Long id;
 
     @JsonIgnore
 	public String getApiLinks() { return getLinks().toString(); } 
 
-    @JsonIgnore
-    public Long getId() {
-        return id;
-    }
+    // @JsonIgnore
+    // public Long getId() {
+    //     return id;
+    // }
     
-    @JsonIgnore
-    public void setId(Long id) {
-        this.id = id;
-    }
+    // @JsonIgnore
+    // public void setId(Long id) {
+    //     this.id = id;
+    // }
 
 }
