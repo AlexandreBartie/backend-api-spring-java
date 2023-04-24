82cd9d05a76571399448b9020f10cf6f1214d104
Alexandre Bartie <bartie.devops@outlook.com>
2023-04-24 00:40:11 -0300

    feat: add integration test

---
+++ src/test/java/br/com/bartie/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,31 @@
+package br.com.bartie.integrationtests.swagger;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.bartie.config.TestConfig;
+import br.com.bartie.integrationtests.testcontainers.AbstractIntegrationTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTest extends AbstractIntegrationTest{
+
+	@Test
+	public void shouldDisplaySwaggerUiPage() {
+		var content =
+			given()
+				.basePath("/swagger-ui/index.html")
+				.port(TestConfig.SERVER_PORT)
+				.when()
+					.get()
+				.then()
+					.statusCode(200)
+				.extract()
+					.body()
+						.asString();
+		assertTrue(content.contains("Swagger UI"));
+	}
+
+}
